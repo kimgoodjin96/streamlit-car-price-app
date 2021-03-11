b8df7eb5cf97a1be136759128bc5e64192f1aab9
kimgoodjin96 <kimgoodjin96@gmail.com>
2021-03-11 13:03:32 +0900

    가격예측

--- eda_app.py
+++ eda_app.py
@@ -27,3 +27,67 @@ def run_eda_app():
     
     elif selected_radio=='통계치':
         st.dataframe(car_df.describe())
+
+
+    car_df = pd.read_csv('data/Car_Purchasing_Data.csv',encoding='ISO-8859-1')
+    columns=car_df.columns
+    columns=list(columns)
+
+    selected_columns=st.multiselect('보고싶은 컬럼을 선택하시오',columns)
+    
+    if len(selected_columns)!=0:
+        st.dataframe(car_df[selected_columns])
+    else:
+        st.write('선택한 컬럼이 없습니다')
+    
+    
+    # 상관계수를 화면에 보여주도록 만듭니다.
+    # 멀티셀렉트에 컬럼명을 보여주고,
+    # 해당 컬럼들에 대한 상관계수를 보여주세요.
+    # 단,컬럼들은 숫자 컬럼들만 멀티셀렉트에 나타나야합니다.
+
+    car_corr=car_df.columns[car_df.dtypes!=object]  
+
+    selected_corr=st.multiselect('상관계수를 보고싶은 컬럼을 선택하세요',car_corr)
+    
+    if len(selected_corr)!=0:
+        st.dataframe(car_df[selected_corr].corr())
+        
+        st.pyplot(sns.pairplot(car_df[selected_corr]))
+    #  위에서 선택한 컬럼들을 이용해서 시본의 페어플롯을 그린다.
+    
+
+    else:
+        st.write('선택한 컬럼이 없습니다')
+
+    
+    # 컬럼을 하나만 선택하면 ,해당 컬럼의 min 과 max에 
+    # 해당하는 사람의 데이터를 화면에 보여주는 기능
+
+    car_minmax=car_df.columns[car_df.dtypes!=object]  
+
+    selected_minmax=st.selectbox('컬럼을 선택해주세요',car_minmax)
+
+  
+    st.write('Max 값에 해당되는 사람의 데이터')
+    Max_customer_data=car_df.loc[car_df[selected_minmax]==car_df[selected_minmax].max(),]
+    st.dataframe(Max_customer_data)
+
+    st.write('Min 값에 해당되는 사람의 데이터')
+    Max_customer_data=car_df.loc[car_df[selected_minmax]==car_df[selected_minmax].min(),]
+    st.dataframe(Max_customer_data)
+    
+
+    # 고객이름을 검색할수 있는 기능 개발
+    search=st.text_input('이름을 입력하세요').lower()
+    # search_data=car_df['Customer Name'].str.lower().to_frame()['Customer Name'].str.contains(search)
+    search_data=car_df['Customer Name'].str.contains(search,case=False)
+    if len(search)!=0:
+        st.dataframe(car_df.loc[search_data,])
+    else:
+        st.write('이름을 입력하세요')
+        
+
+    
+
+
--- ml_app.py
+++ ml_app.py
@@ -18,24 +18,58 @@ import pickle
 import joblib
 
 def run_ml_app():
+
     st.subheader('머신러닝 화면입니다.')
 
+    # 1.유저한테 입력을 받는다.
+    # 성별
+    gender=st.radio('성별을 선택하세요',['남자','여자'])
+    if gender=='남자':
+        gender=1
+    else:
+        gender=0
+    
+    age=st.number_input('나이 입력',min_value=0,max_value=120)
+    salary=st.number_input('연봉 입력',min_value=0)
+    dept=st.number_input('빚 입력',min_value=0)
+    worth=st.number_input('자산 입력',min_value=0)
+
+    # 예측한다
+
+    # 1.모델을 불러온다
     model=tensorflow.keras.models.load_model('data/car_ai.h5')
-
-    new_data=np.array([0,38,90000,2000,500000])
-
+    # 2.넘파이 어레이 만든다.
+    new_data=np.array([gender,age,salary,dept,worth])
+    # 3.피처스케일링 한다.
     new_data=new_data.reshape(1,-1)
-
+    
     sc_X=joblib.load('data/sc_x.pkl')
-
+    
     new_data=sc_X.transform(new_data)
-
+    # 예측한다
     y_pred=model.predict(new_data)
-
-    st.write(y_pred)
-
+    
+    # 예측결과는 스케일링 된 결과이므로 다시 돌려야한다
     sc_y=joblib.load('data/sc_y.pkl')
 
     y_pred_original=sc_y.inverse_transform(y_pred)
 
-    st.write(y_pred_original)
+    btn=st.button('결과 보기')
+    if btn:
+        # st.write('예측된 결과입니다. {:,.1f} 달러의 차를 살수 있습니다.'.format(y_pred_original[0,0]))
+        st.write('예측된 결과입니다. %.1f 달러의 차를 살수 있습니다.' % y_pred_original[0][0])
+
+
+
+   
+
+  
+
+
+    
+    
+
+    
+
+
+  
